f10dbbd5320d05f0cbd8bf291d2f58a190e8a075
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2025-08-29 22:37:20 +0530

    Swap Numbers with second variable.
    Swap Numbers with second variable. (using var temp)

---
+++ swap_with_second_var.c
@@ -0,0 +1,14 @@
+// Write a C Program to Swap Numbers with second variable.
+# include <stdio.h>
+int main(){
+
+    int a = 5, b = 10, temp;
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
