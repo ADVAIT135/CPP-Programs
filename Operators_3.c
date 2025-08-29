aa207e94137b6e0e847317a9aed005b703aed950
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2025-08-29 22:25:41 +0530

    Arithmetic Operators in C
    +, -, /, *, %%

---
+++ Operators_3.c
@@ -0,0 +1,15 @@
+// Operators in C -- 3
+
+# include <stdio.h>
+int main(){
+
+    int a=27, b=7;
+
+    printf("\n a+b = %d", a+b);
+    printf("\n a-b = %d", a-b);
+    printf("\n a*b = %d", a*b);
+    printf("\n a/b = %d", a/b);
+    printf("\n a%%b = %d", a%b);
+
+   return 0;
+}
