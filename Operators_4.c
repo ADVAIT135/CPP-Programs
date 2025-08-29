290a269a5beb964909b7799215491ef1650f1669
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2025-08-29 22:26:25 +0530

    Relational Operators in C
    <
    >
    <=
    >=
    ==
    !=

---
+++ Operators_4.c
@@ -0,0 +1,18 @@
+// Operators in C -- 4
+
+# include <stdio.h>
+
+int main(){
+
+    int x= 12, y = 13;
+    printf("\n X= %d, Y= %d", x, y);
+
+    printf("\n x>y : %d", x>y); // Greater than
+    printf("\n x<y : %d", x<y); // Less than
+    printf("\n x>=y : %d", x>=y); // Greater than or equal to
+    printf("\n x<=y : %d", x<=y); // Less than or equal to
+    printf("\n x==y : %d", x==y); // Equal to
+    printf("\n x!=y : %d", x!=y); // Not equal to
+
+    return 0;
+}
