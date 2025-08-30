d62fde4beef8f8cd84976b29d8b669ad0c032a29
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2025-08-30 09:50:27 +0530

    Switch Statment in C
    Switch Statment in C

---
+++ Conditional Statements and Loops in C/switch_statement.c
@@ -0,0 +1,32 @@
+// Switch Statement in C
+
+# include <stdio.h>
+int main(){
+
+    int x = 3;
+
+    switch (x)
+    {
+    case 1:
+        printf("\n Choice is 1");
+        break;
+    case 2:
+        printf("\n Choice is 2");
+        break;
+    case 3:
+        printf("\n Choice is 3");
+        break;
+    case 4:
+        printf("\n Choice is 4");
+        break;
+    case 5:
+        printf("\n Choice is 5");
+        break;
+    default:
+        printf("\n Choice is other than 1, 2, 3, 4, 5");
+        break;
+    }
+
+    return 0;
+
+}
